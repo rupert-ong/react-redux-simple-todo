25e70b77ef091036c751d270e2d14b6b700f3b07
Rupert Ong <rupert.ong@gmail.com>
2017-09-14 22:37:07 -0400

    Convert data to Immutable JS collections. Add actions.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { TodoList } from './components';
+import { List, Map } from 'immutable';
 
-const dummyTodos = [
-  { id: 0, isDone: true, text: 'make components' },
-  { id: 1, isDone: false, text: 'design actions' },
-  { id: 2, isDone: false, text: 'implement reducer' },
-  { id: 3, isDone: false, text: 'connect components' }
-]
+// Translate dummy data to Immutable JS collections
+const dummyTodos = List([
+  Map({ id: 0, isDone: true, text: 'make components' }),
+  Map({ id: 1, isDone: false, text: 'design actions' }),
+  Map({ id: 2, isDone: false, text: 'implement reducer' }),
+  Map({ id: 3, isDone: false, text: 'connect components'})
+]);
 
 render(
   <TodoList todos={dummyTodos} />, 
