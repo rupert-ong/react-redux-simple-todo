c49958f29d281a1bcb02862673b16d3833c223e3
rupert-ong <rupert.ong@gmail.com>
2017-09-19 22:03:11 -0400

    Remove dummy data.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import { TodoList } from './components';
-import { List, Map } from 'immutable';
+import { Provider } from 'react-redux';
+import { TodoList } from './containers';
 import reducer from './reducer';
-
-// Translate dummy data to Immutable JS collections
-const dummyTodos = List([
-  Map({ id: 0, isDone: true, text: 'make components' }),
-  Map({ id: 1, isDone: false, text: 'design actions' }),
-  Map({ id: 2, isDone: false, text: 'implement reducer' }),
-  Map({ id: 3, isDone: false, text: 'connect components'})
-]);
 
 const store = createStore(reducer);
 
 render(
-  <TodoList todos={store.getState()} />, 
+  <Provider store={store}>
+    <TodoList />
+  </Provider>, 
   document.getElementById('app')
 );
