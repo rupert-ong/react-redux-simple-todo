09f930fad00e21ea5db24fe9a9036cc1a8b3fe6f
rupert-ong <rupert.ong@gmail.com>
2017-09-19 21:36:02 -0400

    Add store aware container wrappers for components

---
+++ src/containers.js
@@ -0,0 +1,18 @@
+// Create store aware containers that wrap around existing components
+// to make components aware of the store
+import { connect } from 'react-redux';
+import * as components from './components';
+import { addTodo, toggleTodo } from './actions';
+
+export const TodoList = connect(
+    function mapStateToProps(state) {
+        return { todos: state };
+    },
+    // Passed to store's dispatch method. Use it to
+    // dispatch actions from our action creators
+    function mapDispatchToProps(dispatch) {
+        return {
+            addTodo: text => dispatch(addTodo(text)),
+            toggleTodo: id => dispatch(toggleTodo(id))
+        };
+    })(components.TodoList);
