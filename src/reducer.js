252c87f3c1b4cfcce6c40330becd36f082099335
Rupert Ong <rupert.ong@gmail.com>
2017-09-20 14:00:47 -0400

    Fix typo in reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,9 @@ import { List, Map } from 'immutable';
 
 const init = List([]);
 
-export default function (todos = init, action) {
+export default function reducer(todos = init, action) {
   switch (action.type) {
-    case 'ADD_TO':
+    case 'ADD_TODO':
       return todos.push(Map(action.payload));
     case 'TOGGLE_TODO':
       return todos.map(t => {
